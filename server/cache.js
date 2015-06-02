cb912ecf8f02d390f61ff67d370f568c36a33bad
Jimmy Gong <gong.jim@gmail.com>
2015-06-02 01:07:31 -0700

    Adds redis cache

---
+++ server/cache.js
@@ -0,0 +1,33 @@
+var redis = require('redis');
+var client = redis.createClient();
+
+client.on('connect', function(){
+  console.log('Connected to REDIS');
+  client.flushdb();
+});
+client.on('error', function(err){
+  console.log('Error:',err);
+});
+
+/** 
+ * Cache methods
+ */ 
+function set(key, val){
+  return client.set(key, val);
+}
+function get(key, callback){
+  return client.get(key, function(err, reply){
+    callback(reply);
+  });
+}
+function contains(key, callback){
+  return client.exists(key, function(err, reply){
+    callback(reply);
+  });
+}
+
+module.exports = {
+  set: set,
+  get: get,
+  contains: contains
+};
